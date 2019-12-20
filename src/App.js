8b3557110dceb4d2b54e59cc6113aa1f26401208
mlazzell17@gmail.com <MicheSi>
2019-12-20 10:02:23 -0700

    added routes, starting on search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+// components
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js";
+// dependencies
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <nav>
+        <div className='nav-links'>
+          <NavLink to='/'>Welcome</NavLink>
+          <NavLink to='/character-list'>Character List</NavLink>
+        </div>
+      </nav>
+      <Switch>
+      <Route path='/character-list'>
+          <CharacterList />
+        </Route>
+        <Route path='/'>
+          <WelcomePage />
+        </Route>
+      </Switch>
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function SearchForm() {
- 
+ const [searchTerm, setSearchTerm] = useState('');
+ const [searchResults, setSearchResults] = useState([]);
+
+
   return (
     <section className="search-form">
      // Add a search form here
